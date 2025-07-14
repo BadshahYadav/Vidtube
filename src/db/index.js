b270d68528eac9763222fc76ca3b6a03d16f5915
Badshah <badshahkumar1508@gmail.com>
2025-07-14 14:06:44 +0530

    Added New-Item in Controllers and routes folder

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,6 @@ import { DB_Name } from "../constant.js";
 const connectDB = async()=>{
     try {
        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_Name}`)
-
        console.log(`\n MongoDB connected ! DB host: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MongoDB connection error", error)
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -31,7 +31,7 @@ router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
-router.route("/updat-account").patch( verifyJWT, updateAccountDetails)
+router.route("/update-account").patch( verifyJWT, updateAccountDetails)
 router.route("/avatar").patch(verifyJWT,upload.single("avatar"), updateUserAvatar);
 router.route("/coverImage").patch(verifyJWT,upload.single("coverImage"), updateUserCoverImage);
 router.route("/history").get(verifyJWT, getWatchHistory);
