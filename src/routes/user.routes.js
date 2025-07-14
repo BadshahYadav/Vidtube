49a232110cbb0150fbc1cc7820f7cd7a95710004
Badshah <badshahkumar1508@gmail.com>
2025-07-14 13:23:36 +0530

    modifiy user.controller.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { registerUser, logoutUser } from "../controllers/user.controllers.js";
+import { registerUser, logoutUser, loginUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, getUserChannelProfile, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getWatchHistory } from "../controllers/user.controllers.js";
 import { upload } from '../middlewares/multer.middlewares.js';
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
+import { get } from "mongoose";
 
 
 const router = Router()
@@ -21,7 +22,18 @@ router.route("/register").post(
     registerUser
 )
 
+router.route("/login").post(loginUser);
+router.route("/refresh-token").post(refreshAccessToken);
+
 //  sercure routes
+
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/updat-account").patch( verifyJWT, updateAccountDetails)
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"), updateUserAvatar);
+router.route("/coverImage").patch(verifyJWT,upload.single("coverImage"), updateUserCoverImage);
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
